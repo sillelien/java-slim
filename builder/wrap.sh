d209fcc9cb283c32b867830bd1bc6ba2ec57273e
neilellis <hello@neilellis.me>
2015-07-31 13:30:46 +0100

    Initial commit

---
+++ builder/wrap.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+set -ex
+tag=$1
+shift
+executable=$1
+shift
+jar=$1
+shift
+resources=$1
+shift
+main=$(echo $1 | tr '.' '/')
+shift
+
+vmargs=${JVM_ARGS:--Xmx128m}
+jremin=${JRE_MINIMIZE:-hard}
+
+java -jar /app/packr.jar \
+     -platform linux64 \
+     -jdk "/app/zulu1.8.0_45-8.7.0.5-x86lx64.zip" \
+     -executable $executable \
+     -appjar /build/$jar \
+     -mainclass $main \
+     -vmargs "$vmargs" \
+     -resources /build/$resources \
+     -minimizejre "$jremin" \
+     -outdir /out
+
+mkdir -p /out
+cd /out
+
+if [ -z "$BASE_IMAGE" ]
+then
+    BASE_IMAGE="sillelien/jess:master"
+fi
+
+echo "Creating Dockerfile for $executable from $BASE_IMAGE" >&2
+cat > Dockerfile <<EOF
+FROM $BASE_IMAGE
+COPY . /app
+WORKDIR /app
+ENTRYPOINT ["/init","/app/$executable"]
+EOF
+
+echo "Building Docker Image $tag" >&2
+
+docker build -t "$tag" .
+
+echo "Built successfully, you can run now with: "  >&2
+echo "$ docker run -ti $tag" >&2
+
+
+
---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -ex
+cd helloworld && mvn clean install -Dmaven.test.skip=true && cd -
+cd builder
+docker build -t java-slim .
+cd -
+cd helloworld
+docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/build java-slim helloworld hello-world target/java-slim.helloworld-1.0-SNAPSHOT.jar src/main/resources sillelien.HelloWorld
+cd -
+docker run -ti helloworld
+
