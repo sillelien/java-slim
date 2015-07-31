d5dd284a47f8b7b593940a17ca77256029d64ffc
neilellis <hello@neilellis.me>
2015-07-31 23:01:27 +0100

    Initial Experiments

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ cd builder
 docker build -t java-slim .
 cd -
 cd helloworld
-docker run -ti -privileged -v /run/docker.sock:/var/run/docker.sock -v $(pwd)/:/build/ java-slim helloworld hello-world target/java-slim.helloworld-1.0-SNAPSHOT.jar src/main/resources sillelien.HelloWorld
+docker run -ti --privileged -v /run/docker.sock:/run/docker.sock -v $(pwd)/:/build/ java-slim helloworld hello-world target/java-slim.helloworld-1.0-SNAPSHOT.jar src/main/resources sillelien.HelloWorld
 cd -
 docker run -ti helloworld
 
