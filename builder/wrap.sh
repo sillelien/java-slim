a233ece6e0ffe4d3f7ac144c3d2adef715fa8799
neilellis <hello@neilellis.me>
2015-07-31 22:45:18 +0100

    Initial Experiments

--- builder/wrap.sh
+++ builder/wrap.sh
@@ -42,7 +42,7 @@ ENTRYPOINT ["/init","/app/$executable"]
 EOF
 
 echo "/etc/default/docker"
-echo 'DOCKER_OPTS="-H unix:///var/run/docker.sock' >>  /etc/default/docker
+echo 'DOCKER_OPTS="-H unix:///var/run/docker.sock"' >>  /etc/default/docker
 #ls -la /var/run/docker.sock
 ls -la /var/run
 echo "Building Docker Image $tag" >&2
